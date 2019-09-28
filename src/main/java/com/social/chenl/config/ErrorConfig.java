9b7fd1a7ad5df289f51c903d6bd114809ae679c4
lance <921358307@qq.com>
2019-09-28 15:59:50 +0800

    ErrorPageRegistrar 修改

--- src/main/java/com/social/chenl/config/ErrorConfig.java
+++ src/main/java/com/social/chenl/config/ErrorConfig.java
@@ -2,7 +2,6 @@ package com.social.chenl.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.server.*;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
@@ -21,9 +20,9 @@ public class ErrorConfig implements ErrorPageRegistrar {
     @Override
     public void registerErrorPages(ErrorPageRegistry registry) {
         ErrorPage error400Page = new ErrorPage(HttpStatus.BAD_REQUEST, servletPath + "400");
-        ErrorPage error401Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, servletPath + "500");
+        ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, servletPath + "500");
         ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, servletPath + "404");
-        registry.addErrorPages(error400Page, error401Page, error404Page);
+        registry.addErrorPages(error400Page, error500Page, error404Page);
     }
 
 
